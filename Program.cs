ddaa07acda6e00f9ba7414e7d89693c94856e613
Anastasiya Mironenko <nastya_911@mail.ru>
2022-12-26 15:45:02 +0300

    написан код для решения задачи найти массивы менее 4 символа

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите слова или символы через пробел:");
+string? array = Console.ReadLine();
+
+string[] miniWords = array.Split(' ');
+Console.WriteLine("Массивы, длиной менее либо равно 3 символа: ");
+
+for (int i = 0; i < miniWords.Length; i++)
+{
+    if (miniWords[i].Length < 4)
+    {
+        Console.Write($"[{miniWords[i]}] ");
+    }
+}
